a5b5c183cd6ca97862d470d5458da1270e6f32ee
Ai-Chen-Lab <chen03810@gmail.com>
2025-02-12 11:01:24 +0800

    Update prepare_patches.py

--- prepare_patches.py
+++ prepare_patches.py
@@ -1,1 +1,13 @@
+from makedataset1 import *
+import argparse
 
+if __name__ == "__main__":
+		
+	parser = argparse.ArgumentParser(description="Building the training patch database")
+	
+	parser.add_argument("--rgb", action='store_true',default = True, help='prepare RGB database instead of grayscale')
+	parser.add_argument("--patch_size", "--p", type=int, default=256, help="Patch size")
+	parser.add_argument("--stride", "--s", type=int, default=224, help="Size of stride")
+	args = parser.parse_args()
+
+	TrainGT('./clear/','./haze/','./low/','./low_haze/','./Depth/',args.patch_size,args.stride)
